5a59439c92fd4e83ddcdba64722f2fa482bc3c44
yokarak <ande67f3@edu.easj.dk>
2020-02-13 11:58:34 +0100

    reformated the code and some small style changes

--- app/src/main/java/com/example/soccer_tracker/MainActivity.java
+++ app/src/main/java/com/example/soccer_tracker/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //using private and setters and getters, since that is what i have been taught is "best practice"
     private int scoreA=0;
     public int getScoreA() { return scoreA; }
     public void setScoreA(int scoreA) { this.scoreA = scoreA; }
+
     private int scoreB=0;
     public int getScoreB() { return scoreB; }
     public void setScoreB(int scoreB) { this.scoreB = scoreB; }
@@ -25,11 +27,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-    }
-
+        setContentView(R.layout.activity_main); }
+        // how the score is show, it it implemented in the goal and foul methods, so it changes when clicked
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score)); }
@@ -44,13 +43,14 @@ public class MainActivity extends AppCompatActivity {
         TextView foulView = (TextView) findViewById(R.id.team_b_fouls);
         foulView.setText(String.valueOf(foul)); }
 
+        //the method for setting and getting the score and foul
     public void goalTeamA (View view) {setScoreA(getScoreA()+1);displayForTeamA(scoreA); }
     public void foulTeamA (View view) {setFoulA(getFoulA()+1); displayFoulTeamA(foulA);}
 
     public void goalTeamB (View view) { setScoreB(getScoreB()+1); displayForTeamB(scoreB);}
     public void foulTeamB (View view) {setFoulB(getFoulB()+1); displayFoulTeamB(foulB);}
 
-
+        //resets all the values and shows the reset values
     public void reset (View view) { setFoulB(0); setFoulA(0); setScoreB(0); setScoreA(0);
     displayFoulTeamB(getFoulB()); displayForTeamB(getScoreB()); displayFoulTeamA(getFoulA()); displayForTeamA(getScoreA()); }
 
